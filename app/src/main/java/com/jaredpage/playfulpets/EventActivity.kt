71d4809d1593e4a3b3d61df149ed4a8f8265c53d
Victor Engelbrecht <victor@warpdevelopment.com>
2022-06-16 22:19:14 +0200

    Some maps page changes

--- app/src/main/java/com/jaredpage/playfulpets/EventActivity.kt
+++ app/src/main/java/com/jaredpage/playfulpets/EventActivity.kt
@@ -40,9 +40,9 @@ class EventActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker at Kenridge Dog Park and move the camera
+        val defaultPark = LatLng(-33.863337380475386, 18.633430856578908)
+        mMap.addMarker(MarkerOptions().position(defaultPark).title("Kenridge Dog Park"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(defaultPark))
     }
 }
